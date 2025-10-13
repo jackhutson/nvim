ad4a569f4a865c5d4acbfd5683a1280c983fad10
Jack Hutson <jack@crossnokaye.com>
2025-10-13 16:06:07 -0400

    feat: fixing ff crash

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,7 +1,7 @@
 return {
   -- LazyGit: Fast terminal UI for git (via snacks.nvim)
   {
-    "snacks.nvim",
+    "folke/snacks.nvim",
     opts = {
       lazygit = {
         configure = true, -- Auto-configure colorscheme integration
@@ -11,7 +11,7 @@ return {
     keys = {
       { "<leader>gn", function() Snacks.lazygit() end, desc = "LazyGit" },
       { "<leader>gN", function() Snacks.lazygit.log() end, desc = "LazyGit Log" },
-      { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "LazyGit File History" },
+      { "<leader>glf", function() Snacks.lazygit.log_file() end, desc = "LazyGit File History" },
       { "<leader>gy", function() Snacks.gitbrowse() end, mode = { "n", "v" }, desc = "Copy Git Link" },
       { "<leader>gY", function() Snacks.gitbrowse({ open = true }) end, mode = { "n", "v" }, desc = "Open Git Link" },
     },
